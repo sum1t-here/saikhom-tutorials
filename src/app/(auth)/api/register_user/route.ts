5bf012de6d1f6f4501e718ff6538750f8a4288e0
sumit <mazumdarsumit3@gmail.com>
2024-11-22 21:55:28 +0530

    added register user api

---
+++ src/app/(auth)/api/register_user/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { username, fullname, email, phone, password } = await req.json();
+
+    if (!username || !fullname || !email || !phone || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 },
+      );
+    }
+
+    // Hash the password asynchronously
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user with hashed password
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        fullname: fullname,
+        email: email,
+        phone: phone,
+        password: hashedPassword,
+      } as const,
+    });
+
+    return NextResponse.json(
+      { user, message: "User created succesfully" },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error handling POST request:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 404 },
+    );
+  }
+}
