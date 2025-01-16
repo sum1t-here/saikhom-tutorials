4774e3f102746738e53b9ab5d3389f5ea574e3d8
sumit <mazumdarsumit3@gmail.com>
2025-01-16 16:35:46 +0530

    add: pdfSchema

---
+++ prisma/migrations/20250116101657_pdf_schema/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "PDF" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PDF_pkey" PRIMARY KEY ("id")
+);
